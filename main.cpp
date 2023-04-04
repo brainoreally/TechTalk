8b69c9a36ccfe0b9cb092e13ff5445122cb2e825
Brian O Reilly <brianoreilly@hotmail.com>
2023-04-04 16:30:38 +0100

    Add code from GPT to show a opengl window drawing a triangle.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,56 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 int main() {
-	std::cout << "Hello World";
-	return 0;
+    // Initialize GLFW
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    // Create a windowed mode window and its OpenGL context
+    GLFWwindow* window = glfwCreateWindow(640, 480, "My OpenGL Window", NULL, NULL);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // Make the window's context current
+    glfwMakeContextCurrent(window);
+
+    // Initialize GLEW
+    GLenum err = glewInit();
+    if (err != GLEW_OK) {
+        std::cerr << "Failed to initialize GLEW" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // Main loop
+    while (!glfwWindowShouldClose(window)) {
+        // Clear the screen
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Draw a triangle
+        glBegin(GL_TRIANGLES);
+        glColor3f(1.0f, 0.0f, 0.0f);    // Red
+        glVertex3f(-0.5f, -0.5f, 0.0f);
+        glColor3f(0.0f, 1.0f, 0.0f);    // Green
+        glVertex3f(0.5f, -0.5f, 0.0f);
+        glColor3f(0.0f, 0.0f, 1.0f);    // Blue
+        glVertex3f(0.0f, 0.5f, 0.0f);
+        glEnd();
+
+        // Swap front and back buffers
+        glfwSwapBuffers(window);
+
+        // Poll for and process events
+        glfwPollEvents();
+    }
+
+    // Clean up
+    glfwTerminate();
+    return 0;
 }
